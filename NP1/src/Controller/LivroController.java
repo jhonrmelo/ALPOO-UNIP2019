8bd320af232980e45eb862f57df6811c7d6b0bba
gabrielpsouza <gabriel.psouza@outlook.com.br>
2019-09-26 09:25:41 -0300

    Criando os métodos para buscar livros e estrutura de conexão com o banco (falta os dados para a conexão)

--- NP1/src/Controller/LivroController.java
+++ NP1/src/Controller/LivroController.java
@@ -1,5 +1,68 @@
 package Controller;
 
 public class LivroController {
-  //teste
+
+    //Favor realizar a conexão (não tenho os dados) - Gabriel
+    public final static String DATABASE = "Teste";
+	public final static String URL = "jdbc:postgresql:" + DATABASE;
+	public final static String USER = "teste";
+	public final static String PASS = "123456";
+    public String filtro;
+
+    //Favor validar - Gabriel
+    //Testa a conexão com o banco antes de buscar os dados  
+    static public void testaConexao() {
+		try(Connection connection = DriverManager.getConnection(URL, USER, PASS)){
+			System.out.println("Conexao feita");
+		} catch(SQLException e) {
+			System.out.println("ERROR: Erro ao tentar conexao");
+			e.printStackTrace();
+		}
+	}
+
+    //Favor validar - Gabriel
+    //Busca Livro sem filtro 
+    static public void listaLivros() {
+
+		final String query = "SELECT * FROM livros";
+
+        try(Connection connection = DriverManager.getConnection(URL, USER, PASS))
+        {
+			Statement stm = connection.createStatement();
+			ResultSet rs = stm.executeQuery(query);
+			while(rs.next()) {
+				System.out.println(
+                "Nome: " + rs.getString(1) + 
+                " Preço: " + rs.getDouble(2) +
+                " Autor: " + rs.getString(3) + 
+                " Editora: "  + rs.getString(4));
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+    }
+
+    //Favor validar - Gabriel
+    //Busca Livro especificos
+    static public void buscaLivro() {
+        
+        final String query = "select * from livros like '" + this.filtro + "'";
+
+        try(Connection connection = DriverManager.getConnection(URL, USER, PASS))
+        {
+            Statement stm = connection.createStatement();
+            ResultSet rs = stm.executeQuery(query);
+            while (rs.next()) 
+            {
+                System.out.println(  
+                "Nome: " + rs.getString(1) + 
+                " Preço: " + rs.getDouble(2) +
+                " Autor: " + rs.getString(3) + 
+                " Editora: "  + rs.getString(4));    
+            }
+        } catch(SQLException e) 
+        {
+            e.printStackTrace();
+        }
+    }
 }
